88bb9255dc6131ba83947d4354c3de201c64b258
Max Thrun <bear24rw@gmail.com>
2013-11-18 01:24:30 -0500

    [test1] final code

--- test_1/cube_frag.glsl
+++ test_1/cube_frag.glsl
@@ -7,28 +7,18 @@ out vec3 out_color;
 
 void main()
 {
-    vec3 light_dir = vec3(0.3,0.9,0.1);
+    vec3 light_dir = vec3(-0.3,0.9,0.1);
     float intensity = dot(light_dir, normalize(Normal));
 
-    //vec3 tmp = vec3(fract(edge.x),fract(edge.y),fract(edge.z));
-    /*
-    float fr = edge.x - floor(edge.x);
-    if (fr < 0.2) {
+    vec3 f = fract(frag_pos);
+
+    if ((f.x < 0.1 || f.x > 0.9) && (f.y < 0.1 || f.y > 0.9) ||
+        (f.x < 0.1 || f.x > 0.9) && (f.z < 0.1 || f.z > 0.9) ||
+        (f.z < 0.1 || f.z > 0.9) && (f.y < 0.1 || f.y > 0.9)) {
         out_color = vec3(0);
     } else {
         out_color = color;
     }
-    */
 
-//    if (any(greaterThanEqual(fract(frag_pos), vec3(0.2))) &&
-        //any(lessThanEqual(fract(frag_pos), vec3(0.8)))) {
-    //if (any(greaterThan(frag_pos.xy, vec2(1)))) {
-    if ((fract(frag_pos.x) < 0.1 || fract(frag_pos.x) > 0.9) && (fract(frag_pos.y) < 0.2 || fract(frag_pos.y) > 0.9) ||
-        (fract(frag_pos.x) < 0.1 || fract(frag_pos.x) > 0.9) && (fract(frag_pos.z) < 0.2 || fract(frag_pos.z) > 0.9) ||
-        (fract(frag_pos.z) < 0.1 || fract(frag_pos.z) > 0.9) && (fract(frag_pos.y) < 0.2 || fract(frag_pos.y) > 0.9)) {
-        out_color = vec3(0);//intensity * 0.1;
-    } else {
-        out_color = color;
-    }
     //out_color = color * intensity;
 }
--- test_1/main.cpp
+++ test_1/main.cpp
@@ -32,6 +32,7 @@ int main()
 {
     sf::ContextSettings contextSettings;
     contextSettings.depthBits = 32;
+    contextSettings.antialiasingLevel = 8;
 
     sf::RenderWindow window(sf::VideoMode(800, 800), "Test 1", sf::Style::Default, contextSettings);
     window.setVerticalSyncEnabled(true);
@@ -68,8 +69,8 @@ int main()
 
     GLuint default_vbo;
     glGenBuffers(1, &default_vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, default_vbo);
-	glBufferData(GL_ARRAY_BUFFER, default_verts.size()*sizeof(float), default_verts.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, default_vbo);
+    glBufferData(GL_ARRAY_BUFFER, default_verts.size()*sizeof(float), default_verts.data(), GL_STATIC_DRAW);
 
     GLuint default_shader_id = LoadShaders("default_vert.glsl", "default_frag.glsl");
     GLint default_mvp_id = glGetUniformLocation(default_shader_id, "mvp");
@@ -107,8 +108,8 @@ int main()
 
     GLuint cube_vbo;
     glGenBuffers(1, &cube_vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, cube_vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cube_verts), cube_verts, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, cube_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cube_verts), cube_verts, GL_STATIC_DRAW);
 
     GLuint cube_shader_id = LoadShaders("cube_vert.glsl", "cube_frag.glsl");
     cube_model_id = glGetUniformLocation(cube_shader_id, "model");
@@ -139,8 +140,8 @@ int main()
 
     // define the translations to assemble each part in their local object coordinate system
     glm::mat4 cam_hing_mcs   = glm::translate(glm::mat4(1.0f), glm::vec3( 0.0f,  0.0f,  0.0f));
-    glm::mat4 cam_body_mcs   = glm::translate(glm::mat4(1.0f), glm::vec3( 1.0f, -3.0f, -2.0f));
-    glm::mat4 cam_lens_mcs   = glm::translate(glm::mat4(1.0f), glm::vec3( 1.5f, -4.0f,  0.0f));
+    glm::mat4 cam_body_mcs   = glm::translate(glm::mat4(1.0f), glm::vec3( 1.0f, -3.0f, -1.5f));
+    glm::mat4 cam_lens_mcs   = glm::translate(glm::mat4(1.0f), glm::vec3( 1.5f, -4.0f, -0.5f));
     glm::mat4 shaft_hing_scs = glm::translate(glm::mat4(1.0f), glm::vec3( 0.5f, 15.0f,  2.0f));
     glm::mat4 shaft_body_scs = glm::translate(glm::mat4(1.0f), glm::vec3( 0.0f,  0.0f,  0.0f));
     glm::mat4 base_body_bcs  = glm::translate(glm::mat4(1.0f), glm::vec3( 0.0f,  0.0f,  0.0f));
@@ -169,13 +170,15 @@ int main()
     glm::mat4 base_hing_ocs = base_ocs * base_hing_bcs;
     glm::mat4 base_body_ocs = base_ocs * base_body_bcs;
 
-    glm::mat4 shaft_ocs = glm::rotate(glm::mat4(1.0f), 30.0f, glm::vec3(1,0,0));
+    glm::mat4 shaft_ocs = glm::mat4(1.0f);
+              //shaft_ocs = glm::rotate(shaft_ocs, 30.0f, glm::vec3(1,0,0));
               shaft_ocs = base_hing_ocs * shaft_ocs;
               shaft_ocs = glm::translate(shaft_ocs, glm::vec3(2.0f, 0.0f, 2.0f));
     glm::mat4 shaft_hing_ocs = shaft_ocs * shaft_hing_scs;
     glm::mat4 shaft_body_ocs = shaft_ocs * shaft_body_scs;
 
-    glm::mat4 cam_ocs = glm::rotate(glm::mat4(1.0f), -45.0f, glm::vec3(1,0,0));
+    glm::mat4 cam_ocs = glm::mat4(1.0f);
+              //cam_ocs = glm::rotate(cam_ocs, -45.0f, glm::vec3(1,0,0));
               cam_ocs = shaft_hing_ocs * cam_ocs;
               cam_ocs = glm::translate(cam_ocs, glm::vec3(2.0f, 0.0f, 0.0f));
     glm::mat4 cam_lens_ocs = cam_ocs * cam_lens_mcs;
@@ -203,7 +206,6 @@ int main()
     glm::vec3 camera_target(15.0f, 25.0f, 24.0f);
 
     projection = glm::perspective(45.0f, 1.0f, 0.1f, 1000.0f);
-	//glm::mat4 projection = glm::ortho(-100.0f, 100.0f, -100.0f, 100.0f, 1.0f, 1000.0f);
 
     int poly_mode = 0;
 
@@ -234,10 +236,10 @@ int main()
                         camera_angle_h += (M_PI/ 4);
                         break;
                     case sf::Keyboard::Up:
-                        camera_angle_v -= (M_PI / 4);
+                        camera_angle_v += (M_PI / 4);
                         break;
                     case sf::Keyboard::Down:
-                        camera_angle_v += (M_PI / 4);
+                        camera_angle_v -= (M_PI / 4);
                         break;
                     case sf::Keyboard::PageUp:
                         camera_dist *= 0.9f;
@@ -285,8 +287,8 @@ int main()
                 glm::vec3(0,1,0)        // up
                 );
 
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glClearColor(0.8, 0.8, 0.8, 0.0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(1.0, 1.0, 1.0, 0.0);
 
         if (poly_mode) {
             glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -299,52 +301,37 @@ int main()
         //
         // Draw the body
         //
-        glm::vec3 color;
 
-		glUseProgram(cube_shader_id);
-
+        glUseProgram(cube_shader_id);
         glBindVertexArray(cube_vao);
         glBindBuffer(GL_ARRAY_BUFFER, cube_vbo);
 
-        /*
-        glm::mat4 cam_lens_mvp   = cam_lens_wcs   * cam_lens;
-        glm::mat4 cam_body_mvp   = cam_body_wcs   * cam_body;
-        glm::mat4 cam_hing_mvp   = cam_hing_wcs   * cam_hing;
-        glm::mat4 shaft_hing_mvp = shaft_hing_wcs * shaft_hing;
-        glm::mat4 shaft_body_mvp = shaft_body_wcs * shaft_body;
-        glm::mat4 base_hing_mvp  = base_hing_wcs  * base_hing;
-        glm::mat4 base_body_mvp  = base_body_wcs  * base_body;
-        glm::mat4 desk_top_mvp   = desk_top_wcs   * desk_top;
-        glm::mat4 desk_leg_1_mvp = desk_leg_1_wcs * desk_leg_1;
-        glm::mat4 desk_leg_2_mvp = desk_leg_2_wcs * desk_leg_2;
-        glm::mat4 desk_leg_3_mvp = desk_leg_3_wcs * desk_leg_3;
-        glm::mat4 desk_leg_4_mvp = desk_leg_4_wcs * desk_leg_4;
+        // define each components final model matrix (mm)
+        glm::mat4 cam_lens_mm   = cam_lens_wcs   * cam_lens;
+        glm::mat4 cam_body_mm   = cam_body_wcs   * cam_body;
+        glm::mat4 cam_hing_mm   = cam_hing_wcs   * cam_hing;
+        glm::mat4 shaft_hing_mm = shaft_hing_wcs * shaft_hing;
+        glm::mat4 shaft_body_mm = shaft_body_wcs * shaft_body;
+        glm::mat4 base_hing_mm  = base_hing_wcs  * base_hing;
+        glm::mat4 base_body_mm  = base_body_wcs  * base_body;
+        glm::mat4 desk_top_mm   = desk_top_wcs   * desk_top;
+        glm::mat4 desk_leg_1_mm = desk_leg_1_wcs * desk_leg_1;
+        glm::mat4 desk_leg_2_mm = desk_leg_2_wcs * desk_leg_2;
+        glm::mat4 desk_leg_3_mm = desk_leg_3_wcs * desk_leg_3;
+        glm::mat4 desk_leg_4_mm = desk_leg_4_wcs * desk_leg_4;
 
-        draw_cube(cam_lens_mvp   , glm::vec3(0.0f, 0.0f, 1.0f));
-        draw_cube(cam_body_mvp   , glm::vec3(0.0f, 1.0f, 0.0f));
-        draw_cube(cam_hing_mvp   , glm::vec3(0.0f, 1.0f, 1.0f));
-        draw_cube(shaft_hing_mvp , glm::vec3(1.0f, 0.0f, 0.0f));
-        draw_cube(shaft_body_mvp , glm::vec3(1.0f, 0.0f, 1.0f));
-        draw_cube(base_hing_mvp  , glm::vec3(1.0f, 1.0f, 0.0f));
-        draw_cube(base_body_mvp  , glm::vec3(1.0f, 1.0f, 0.5f));
-        draw_cube(desk_top_mvp   , glm::vec3(0.5f, 0.0f, 0.5f));
-        draw_cube(desk_leg_1_mvp , glm::vec3(0.5f, 0.0f, 0.5f));
-        draw_cube(desk_leg_2_mvp , glm::vec3(0.5f, 0.0f, 0.5f));
-        draw_cube(desk_leg_3_mvp , glm::vec3(0.5f, 0.0f, 0.5f));
-        draw_cube(desk_leg_4_mvp , glm::vec3(0.5f, 0.0f, 0.5f));
-        */
-
-        glm::mat4 cam_lens_mvp = cam_lens_mcs   * cam_lens;
-        glm::mat4 cam_body_mvp = cam_body_mcs   * cam_body;
-        glm::mat4 cam_hing_mvp = cam_hing_mcs   * cam_hing;
-        draw_cube(cam_lens_mvp   , glm::vec3(0.0f, 0.0f, 1.0f));
-        draw_cube(cam_body_mvp   , glm::vec3(0.0f, 1.0f, 0.0f));
-        draw_cube(cam_hing_mvp   , glm::vec3(0.0f, 1.0f, 1.0f));
-
-        /*
-        glm::mat4 desk_top_mvp = desk_top_dcs * desk_top;
-        draw_cube(desk_top_mvp, view, projection, glm::vec3(0.5f, 0.0f, 0.5f));
-        */
+        draw_cube(cam_lens_mm   , glm::vec3(0.0f, 0.0f, 1.0f));
+        draw_cube(cam_body_mm   , glm::vec3(0.0f, 1.0f, 0.0f));
+        draw_cube(cam_hing_mm   , glm::vec3(0.0f, 1.0f, 1.0f));
+        draw_cube(shaft_hing_mm , glm::vec3(1.0f, 0.0f, 0.0f));
+        draw_cube(shaft_body_mm , glm::vec3(1.0f, 0.0f, 1.0f));
+        draw_cube(base_hing_mm  , glm::vec3(1.0f, 0.5f, 0.0f));
+        draw_cube(base_body_mm  , glm::vec3(1.0f, 1.0f, 0.5f));
+        draw_cube(desk_top_mm   , glm::vec3(0.5f, 0.0f, 1.0f));
+        draw_cube(desk_leg_1_mm , glm::vec3(0.5f, 0.0f, 0.5f));
+        draw_cube(desk_leg_2_mm , glm::vec3(0.5f, 0.0f, 0.5f));
+        draw_cube(desk_leg_3_mm , glm::vec3(0.5f, 0.0f, 0.5f));
+        draw_cube(desk_leg_4_mm , glm::vec3(0.5f, 0.0f, 0.5f));
 
         //
         // Draw the grid
