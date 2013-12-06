d6e1a581d716ff527e0c015a3044f52987846a61
Max Thrun <bear24rw@gmail.com>
2013-12-06 17:57:37 -0500

    [final] report

--- final/code/head_frag.glsl
+++ final/code/head_frag.glsl
@@ -18,45 +18,27 @@ uniform vec3 light_pos_world;
 void main(){
 
 	// Light emission properties
-	// You probably want to put them as uniforms
 	vec3 light_color = vec3(1,1,1);
 	float light_power = 1500.0f;
 
 	// Material properties
-	vec3 MaterialDiffuseColor = texture2D(tex, UV ).rgb;
-	vec3 MaterialAmbientColor = vec3(0.1,0.1,0.1) * MaterialDiffuseColor;
-	vec3 MaterialSpecularColor = vec3(0.3,0.3,0.3);
+	vec3 diffuse_color = texture2D(tex, UV ).rgb;
+	vec3 ambient_color = vec3(0.1,0.1,0.1) * diffuse_color;
+	vec3 specular_color = vec3(0.3,0.3,0.3);
 
 	// Distance to the light
 	float distance = length(light_pos_world - vert_pos_world );
 
-	// Normal of the computed fragment, in camera space
 	vec3 n = normalize( vert_nor_cam );
-	// Direction of the light (from the fragment to the light)
 	vec3 l = normalize( light_dir_cam );
-	// Cosine of the angle between the normal and the light direction,
-	// clamped above 0
-	//  - light is at the fragical of the triangle -> 1
-	//  - light is perpendicular to the triangle -> 0
-	//  - light is behind the triangle -> 0
 	float cosTheta = clamp( dot( n,l ), 0,1 );
 
-	// Eye vector (towards the camera)
 	vec3 E = normalize(eye_dir_cam);
-	// Direction in which the triangle reflects the light
 	vec3 R = reflect(-l,n);
-	// Cosine of the angle between the Eye vector and the Reflect vector,
-	// clamped to 0
-	//  - Looking into the reflection -> 1
-	//  - Looking elsewhere -> < 1
 	float cosAlpha = clamp( dot( E,R ), 0,1 );
 
-	color =
-		// Ambient : simulates indirect lighting
-		MaterialAmbientColor +
-		// Diffuse : "color" of the object
-		MaterialDiffuseColor * light_color * light_power * cosTheta / (distance*distance) +
-		// Specular : reflective highlight, like a mirror
-		MaterialSpecularColor * light_color * light_power * pow(cosAlpha,5) / (distance*distance);
+	color = ambient_color +
+            diffuse_color * light_color * light_power * cosTheta / (distance*distance) +
+            specular_color * light_color * light_power * pow(cosAlpha,5) / (distance*distance);
 
 }
